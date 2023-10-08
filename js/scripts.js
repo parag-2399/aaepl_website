e9ee0c0359284959a9e1a8e7b05293298ab10ae9
parag <parabparag727@gmail.com>
2023-10-08 21:49:21 +0530

    added clients

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,37 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+// -------------------------------------------------------------------
+
+
+
+$(document).ready(function(){
+    // Initiate the wowjs
+    new WOW().init();
+
+    
+    $(".brands-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        },
+        nav: false,
+        dots: false
+    });
+    
+});
